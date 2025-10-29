3204880d83482d0bac76bfb1735af0ace00b5e18
Kevin Leach <kevin.leach@vanderbilt.edu>
2025-10-29 15:39:17 -0500

    testing more

--- custom.js
+++ custom.js
@@ -5,10 +5,15 @@ setInterval(() => {
 
   // force selection of desktop workspace   TODO do the ports change?
   // 6080 was in the workspace for me
-  if (curselected != "desktop: 6080") {
-    // should be a function in actionbar.js...   
-    selectService("desktop: 6080");
-  }
+ 
+  if ($("#workspace-select").length) {
+    if ($("#workspace-select").val() != "desktop: 6080") {
+      if (typeof selectService === "function") {
+        // force selection if we find the workspace selector menu and
+        // the current selection is not the desktop
+        selectService("desktop: 6080");
+      }
+    }
 
   console.log("Custom JS in web-security" + curselected + "\n");
 }, 1000);
